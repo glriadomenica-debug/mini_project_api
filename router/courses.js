29845a04ffd0923cd5a9dcef326597d84c891df3
glriadomenica-debug <glriadomenica@gmail.com>
2026-04-07 16:33:30 +0700

    Update courses.js

--- router/courses.js
+++ router/courses.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const course = require("../controller/course_controller");
+const validateCourse = require("../middleware/validateCourse");
+const validateAuth = require("../middleware/validateAuth");
 
-router.get("/with-relations", course.getAllWithRelation);
+router.get(
+  "/with-relations",
+  validateAuth.validateToken,
+  course.getAllWithRelation,
+);
 router.get("/", course.getAll);
 router.get("/:id", course.getByID);
-router.post("/", course.store);
-router.put("/:id", course.update);
-router.delete("/:id", course.destroy);
+router.post("/", validateAuth.validateToken, validateCourse, course.store);
+router.put("/:id", validateAuth.validateToken, course.update);
+router.delete("/:id", validateAuth.validateToken, course.destroy);
 
 module.exports = router;
+ 
