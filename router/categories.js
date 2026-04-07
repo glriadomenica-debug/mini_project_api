22aab9a6b3abd9ab16c1e1aab2a69cc31285f73c
glriadomenica-debug <glriadomenica@gmail.com>
2026-04-07 15:08:26 +0700

    Update categories.js

--- router/categories.js
+++ router/categories.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const category = require("../controller/category_controller");
+const validateCategory = require("../middleware/validateCategory");
+const validateAuth = require("../middleware/validateAuth");
 
-router.get("/", category.getAll);
+router.get("/", validateAuth.validateToken, category.getAll);
 router.get("/:id", category.getByID);
 router.post("/", category.store);
 router.put("/:id", category.update);
 router.delete("/:id", category.destroy);
 
 module.exports = router;
+  
