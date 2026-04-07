66e01691e46fd8310ab8d97222751cdc3f7412e3
glriadomenica-debug <glriadomenica@gmail.com>
2026-04-07 16:29:26 +0700

    Create validateCourse.js

---
+++ middleware/validateCourse.js
@@ -0,0 +1,27 @@
+const { body } = require("express-validator");
+
+const validateCourse = [
+  body("nama_course").notEmpty().withMessage("Course name is required"),
+  body("price")
+    .notEmpty()
+    .withMessage("Price is required")
+    .isFloat()
+    .withMessage("Must be a number"),
+  body("kuota")
+    .notEmpty()
+    .withMessage("Kuota is required")
+    .isNumber()
+    .withMessage("Must be a number"),
+  body("id_category")
+    .notEmpty()
+    .withMessage("ID category is required")
+    .isInt()
+    .withMessage("Must be a number"),
+  body("id_users")
+    .notEmpty()
+    .withMessage("ID users is required")
+    .isInt()
+    .withMessage("Must be a number"),
+];
+
+module.exports = validateCourse;
