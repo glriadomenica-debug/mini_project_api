70010cc132de82763c02388fd1876b7ddd794143
glriadomenica-debug <glriadomenica@gmail.com>
2026-04-07 17:01:21 +0700

    Update users.js

--- router/users.js
+++ router/users.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../controller/user_controller");
+const validateUser = require("../middleware/validateUser");
+const validateAuth = require("../middleware/validateAuth");
 
-router.get("/", user.getAll);
-router.get("/:id", user.getByID);
-router.post("/", user.store);
-router.put("/:id", user.update);
-router.delete("/:id", user.destroy);
+router.get("/", validateAuth.validateToken,user.getAll);
+router.get("/:id",validateAuth.validateToken, user.getByID);
+router.post("/", validateAuth.validateToken,validateUser,user.store);
+router.put("/:id", validateAuth.validateToken,user.update); 
+router.delete("/:id", validateAuth.validateToken, user.destroy);
 
-module.exports = router;
+module.exports = router; 
