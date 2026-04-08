d91925a5cf308e282ad2b792911d7381fb732570
glriadomenica-debug <glriadomenica@gmail.com>
2026-04-08 22:59:05 +0700

    Update users.js

--- router/users.js
+++ router/users.js
@@ -4,8 +4,8 @@ const user = require("../controller/user_controller");
 const validateUser = require("../middleware/validateUser");
 const validateAuth = require("../middleware/validateAuth");
 
-router.get("/", validateAuth.validateToken,user.getAll);
-router.get("/:id",validateAuth.validateToken, user.getByID);
+router.get("/", user.getAll);
+router.get("/:id", user.getByID);
 router.post("/", validateAuth.validateToken,validateUser,user.store);
 router.put("/:id", validateAuth.validateToken,user.update); 
 router.delete("/:id", validateAuth.validateToken, user.destroy);
