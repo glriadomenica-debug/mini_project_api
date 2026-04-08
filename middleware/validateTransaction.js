5434d0bde1363e1e7b998e71a612d97245de1223
glriadomenica-debug <glriadomenica@gmail.com>
2026-04-08 12:46:37 +0700

    Update validateTransaction.js

--- middleware/validateTransaction.js
+++ middleware/validateTransaction.js
@@ -1,8 +1,21 @@
 const { body } = require("express-validator");
 
 const validateTransaction = [
-  body("id_user").notEmpty().withMessage("User id is required").isInt().withMessage("Must be a number"),
-  body("id_course").notEmpty.withMessage("").isInt().withMessage(""),
-  body("total_price").notEmpty.withMessage("").isNumber().withMessage(""),
-  body("status").notEmpty.withMessage("").isString().withMessage("")
-]
+  body("id_user")
+    .notEmpty()
+    .withMessage("User id is required")
+    .isInt()
+    .withMessage("Must be a number"),
+  body("id_course")
+    .notEmpty.withMessage("Course id is required")
+    .isInt()
+    .withMessage("Must be a number"),
+  body("total_price")
+    .notEmpty.withMessage("Total price is required")
+    .isNumber()
+    .withMessage("Must be a number"),
+  body("status")
+    .notEmpty.withMessage("Status is required")
+    .isString()
+    .withMessage("'Paid'or'Pending'"),
+];
